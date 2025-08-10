f0d6bc11cbe9185b85fffa2f1da99b875d904fed
hexaredecimal <mfanakagama@gmail.com>
2025-08-10 23:14:30 +0200

    PiccodePluginPanel: Add switching colors and a header to the plugins

--- src/main/java/com/piccode/piccodeplugin/PiccodePluginPanel.java
+++ src/main/java/com/piccode/piccodeplugin/PiccodePluginPanel.java
@@ -1,6 +1,14 @@
 package com.piccode.piccodeplugin;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Cursor;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
@@ -8,8 +16,15 @@ import javax.swing.JPanel;
  * @author hexaredecimal
  */
 public class PiccodePluginPanel extends JPanel implements PiccodePluginInterface {
+
 	protected PiccodePluginPanel instance;
 	protected Component editor;
+	private JPanel headerPanel;
+	private List<JLabel> labels = new ArrayList<>();
+
+	public static Color PLUGIN_TITLE_BG = Color.WHITE;
+	public static Color PLUGIN_TITLE_FG = Color.BLACK;
+
 	public PiccodePluginPanel() {
 		super();
 		editor = null;
@@ -18,6 +33,33 @@ public class PiccodePluginPanel extends JPanel implements PiccodePluginInterface
 	@Override
 	public void init() {
 		instance = new PiccodePluginPanel();
+
+		headerPanel = new JPanel();
+		headerPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		// headerPanel.setLayout(new BoxLayout(headerPanel, BoxLayout.X_AXIS)); // Use BoxLayout for horizontal arrangement
+		headerPanel.setBackground(PLUGIN_TITLE_BG);
+		var cmds = new String[]{};
+		for (String command : cmds) {
+
+			if (command.equals("_")) {
+				JLabel sep_lbl = new JLabel("| ");
+				labels.add(sep_lbl);
+				sep_lbl.setForeground(PLUGIN_TITLE_FG);
+				headerPanel.add(sep_lbl);
+				continue;
+			}
+
+			JLabel commandLabel = new JLabel(command + "  ");
+			labels.add(commandLabel);
+			var fnt = commandLabel.getFont();
+			fnt = new Font(fnt.getFontName(), fnt.getStyle(), 9);
+			commandLabel.setFont(fnt);
+			commandLabel.setForeground(PLUGIN_TITLE_FG);
+			commandLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			headerPanel.add(commandLabel);
+		}
+
+		instance.add(headerPanel, BorderLayout.NORTH);
 	}
 
 	@Override
@@ -32,5 +74,15 @@ public class PiccodePluginPanel extends JPanel implements PiccodePluginInterface
 
 	@Override
 	public void setThemeMode(boolean dark) {
+		if (dark) {
+			PLUGIN_TITLE_BG = Color.WHITE;
+			PLUGIN_TITLE_FG = Color.BLACK;
+		} else {
+			PLUGIN_TITLE_BG = Color.WHITE;
+			PLUGIN_TITLE_FG = Color.BLACK;
+		}
+
+		headerPanel.setBackground(PLUGIN_TITLE_BG);
+		labels.forEach(label -> label.setForeground(PLUGIN_TITLE_FG));
 	}
 }
